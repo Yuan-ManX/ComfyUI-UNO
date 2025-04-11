8ecdceb64cd696ea0c2d8e4e838682cdf51eaf11
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-11 15:37:32 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
+from .nodes import ImagePathLoader, UNOParams, UNOGenerator, ImageConcat, ImageSave, ConfigSave
 
+NODE_CLASS_MAPPINGS = {
+    "ImagePathLoader": ImagePathLoader,
+    "UNOParams": UNOParams,
+    "UNOGenerator": UNOGenerator,
+    "ImageConcat": ImageConcat,
+    "ImageSave": ImageSave,
+    "ConfigSave": ConfigSave,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ImagePathLoader": "Image Path Loader",
+    "UNOParams": "UNO Params",
+    "UNOGenerator": "UNO Generator",
+    "ImageConcat": "Image Concat",
+    "ImageSave": "Image Save",
+    "ConfigSave": "Config Save",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
